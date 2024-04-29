e7234a5793498cba4f706c84499501091c988b63
ProfCordasco <161847596+ProfCordasco@users.noreply.github.com>
2024-04-29 15:25:00 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,24 @@
-
+function doLogin(event) {
+  var username = document.getElementById("username").value;
+  var psw = document.getElementById("password").value;
+  if(username == "" || psw == ""){
+    alert("username e password sono obbligatori");
+  }
+  else {
+  fetch("https://www.cacciaapi.altervista.org/login.php/loginIpad", {
+   method: "POST", 
+   headers: {
+    "Content-Type": "application/json",
+   },
+   body: JSON.stringify({
+    "username": username,
+    "password": password
+   )}
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch((error) => {
+    console.error("Errore:", error);
+  });
+ }
+}
